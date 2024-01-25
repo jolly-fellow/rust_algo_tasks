b7c02e867cc2c68396c4ee1b3d7d5b36182b4c2e
Alexander Molchevsky <molchevsky@gmail.com>
2024-01-25 14:58:47 +0200

    Added 70.Climbing Stairs solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,64 @@
 /*
 https://leetcode.com/problems/calculate-money-in-leetcode-bank/description/
- */
+*/
+
 pub fn leetcode_1716(n: i32) -> i32 {
     let k = n / 7; // number of weeks
     let m = n % 7; // rest of the last week
-    7 * ((k * (k + 1) / 2) + 3 * k) + (m * (m + 1) / 2) + m * k
+    let w = 7 * ((k * (k + 1) / 2) + 3 * k); // sum for full weeks
+    let p = (m * (m + 1) / 2) + m * k; // sum for partial week
+    w + p
+}
+
+/*
+ https://leetcode.com/problems/climbing-stairs
+
+Actually we need to calcolate the Fibonacci sequence because number of
+possible ways to up to the stairs is sum of N+1 stairs and N+2 stairs where N is a number of stairs
+
+n can't be more than 45 amd less than 0
+
+*/
+
+//     Recursive solution
+pub fn leetcode_70(n: i32) -> i32 {
+    if n < 4 {
+        return n;
+    }
+    leetcode_70(n - 1) + leetcode_70(n - 2)
+}
+
+//     Iterative solution with array
+pub fn leetcode_70_array(n: usize) -> usize {
+    if n < 4 {
+        n
+    } else {
+        let mut v: [usize; 46] = [0; 46];
+        v[0] = 1;
+        v[1] = 2;
+        for i in 2..=n {
+            v[i] = v[i - 1] + v[i - 2];
+        }
+        v[n - 1]
+    }
+}
+
+//     Iterative solution full optimized
+pub fn leetcode_70_full(n: usize) -> usize {
+    if n < 4 {
+        return n;
+    }
+
+    let mut prev1 = 1;
+    let mut prev2 = 2;
+    let mut ways = 0;
+
+    for _ in 2..n {
+        ways = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = ways;
+    }
+    ways
 }
 
 #[cfg(test)]
@@ -19,5 +73,17 @@ mod tests {
         assert_eq!(result, 37);
         let result = leetcode_1716(20);
         assert_eq!(result, 96);
+    }
+    #[test]
+    fn test_leetcode_70() {
+        assert_eq!(leetcode_70(20), 10946);
+    }
+    #[test]
+    fn test_leetcode_70_array() {
+        assert_eq!(leetcode_70_array(20), 10946);
+    }
+    #[test]
+    fn test_leetcode_70_full() {
+        assert_eq!(leetcode_70_full(20), 10946);
     }
 }
